c35dc64682beabb8df9f72fa39c09d31f09cf6ac
Ronin <roninnvms@gmail.com>
2026-04-14 05:07:13 +0530

    fix: port fix

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ ConnectDB();
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
