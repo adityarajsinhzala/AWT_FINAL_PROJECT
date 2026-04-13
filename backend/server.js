bc854f0bd1ed3c19d085ce8266f137600f34fbb3
Ronin <roninnvms@gmail.com>
2026-04-14 05:15:00 +0530

    fix: server.js fix

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,42 @@
-import express from "express"
+import express from "express";
 import { ConnectDB } from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
-import cors from "cors"
+import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const app = express();
 
-app.use("/uploads", express.static(path.resolve("uploads")));
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }))
 app.use(express.json());
 
-ConnectDB();
+app.use("/uploads", express.static(path.resolve("uploads")));
 
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server running on port 5000");
-});
+
+const startServer = async () => {
+  try {
+    await ConnectDB();
+
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
+
+  } catch (err) {
+    console.error("Server startup error:", err);
+  }
+};
+
+startServer();
